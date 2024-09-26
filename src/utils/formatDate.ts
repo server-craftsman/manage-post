c9c5cf098380a8c343f79755ac7a206de01c342f
server-craftsman <huyit2003@gmail.com>
2024-09-26 17:24:13 +0700

    delete index in admin components

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,7 @@
 export const formatDate = (date: Date | null): string => {
-  if (!date || isNaN(date.getTime())) return 'N/A'; // Check for invalid date
+  if (!(date instanceof Date) || isNaN(date.getTime())) {
+    return 'N/A'; // Check for invalid date
+  }
   return date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
