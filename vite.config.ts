3cf1bfc9d07f667697e5d3854602546b02a3db46
server-craftsman <huyit2003@gmail.com>
2024-09-27 21:36:34 +0700

    build config v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+/// <reference types="node" />
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [react()],
   server: {
     port: 3000,
@@ -27,4 +29,7 @@ export default defineConfig({
     },
     chunkSizeWarningLimit: 1000, // Adjust the chunk size warning limit if needed
   },
+  define: {
+    'process.env': process.env
+  }
 })
